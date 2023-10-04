dbee208bc51275a5977030e1a98bf698632c051f
Josh Chen <joshchen2003@gmail.com>
2023-10-04 17:53:49 -0400

    basic endpoint setup

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from .db import DB
+# from .db import DB
+from werkzeug.urls import url_quote
 
 # Create the Flask app
 app = Flask(__name__)
-app.db = DB(app)
+# app.db = DB(app)
 
 
+@app.route('/')
+def get():
+    return 'Pikadle API'
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
