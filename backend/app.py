8d2be98db5facab676f48c377b7f6115ece836a9
Josh Chen <joshchen2003@gmail.com>
2023-10-04 18:53:26 -0400

    set up elephantsql db

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
-# from .db import DB
-from werkzeug.urls import url_quote
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
 
 # Create the Flask app
 app = Flask(__name__)
-# app.db = DB(app)
-
+url = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(url)
 
 @app.route('/')
 def get():
     return 'Pikadle API'
+
+
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
