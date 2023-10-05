8051c763e99eac1d6ceda86b6e98bf1ef3be3060
Joshua Chen <joshchen2003@gmail.com>
2023-10-05 02:54:26 -0400

    Backend setup + First API endpoint (#10)
    * basic endpoint setup
    
    * set up elephantsql db
    
    * added gitignore
    
    * removed file
    
    * created api route
    
    * changes
    
    * Update app.py
    
    * test
    
    * test
    
    * trying to load in data
    
    * first endpoint
    
    * cleaned db and finished first endpoint
    
    * removed scripts to load data
    
    ---------
    
    Co-authored-by: Will Yun <william.yun@duke.edu>

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,30 @@
+from sqlite3 import Cursor
 from flask import Flask
-from .db import DB
+from flask_restful import Api, Resource
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
 
 # Create the Flask app
 app = Flask(__name__)
-app.db = DB(app)
+api = Api(app)
+url = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(url)
 
+@app.route('/')
+def get():
+    return 'Pikadle API'
 
+import db.Pokemon as Pokemon
+
+class Main(Resource):
+    def get(self):
+        return "pikadle API", 200
+
+api.add_resource(Main, '/')
+api.add_resource(Pokemon.Pokemon, '/pokemon/<int:pokeID>')
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
