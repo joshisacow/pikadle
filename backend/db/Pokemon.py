662bbf57142c37227ce715672d6917f9a043ef4d
Joshua Chen <joshchen2003@gmail.com>
2023-10-05 03:11:18 -0400

    finished random endpoint (#11)

--- backend/db/Pokemon.py
+++ backend/db/Pokemon.py
@@ -36,3 +36,32 @@ class Pokemon(Resource):
             return pokemon_data, 200
         else:
             return {"message": "Pokemon not found"}, 404
+        
+
+class Random(Resource):
+    def get(self):
+        # Get a random pokemon from the database
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM pokemon ORDER BY RANDOM() LIMIT 1")
+        pokemon = cur.fetchone()
+        cur.close()
+        if pokemon:
+            pokemon_data = {
+                "pokemon_id": int(pokemon[0]),
+                "generation": int(pokemon[1]),
+                "type1": str(pokemon[2]),
+                "type2": str(pokemon[3]),
+                "health": int(pokemon[4]),
+                "special_attack": int(pokemon[5]),
+                "attack": int(pokemon[6]),
+                "defense": int(pokemon[7]),
+                "special_defense": int(pokemon[8]),
+                "speed": int(pokemon[9]),
+                "height": float(pokemon[10]),
+                "weight": float(pokemon[11]),
+                "name": str(pokemon[12])
+                # Add other fields as needed
+            }
+            return pokemon_data, 200
+        else:
+            return {"message": "Pokemon not found"}, 404
