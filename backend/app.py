72707730151996fee7831bdc0b28fecb6fc3ac99
Josh Chen <joshchen2003@gmail.com>
2023-10-05 02:51:41 -0400

    cleaned db and finished first endpoint

--- backend/app.py
+++ backend/app.py
@@ -13,53 +13,6 @@ api = Api(app)
 url = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(url)
 
-cursor = conn.cursor 
-
-sql = '''CREATE TABLE Pokemon (
-    pokemon_id INT PRIMARY KEY,
-    generation INT,
-    type1 VARCHAR(255),
-    type2 VARCHAR(255),
-    health INT,
-    special_attack INT,
-    attack INT,
-    defense INT,
-    special_defense INT,
-    speed INT,
-    height DECIMAL(5, 2),
-    weight DECIMAL(5, 2)
-);'''
-
-cursor.execute(sql)
-
-sql2 = '''LOAD DATA INFILE '/Users/williamyun/pikadle/backend/db/data/pokemon.csv'  
-INTO TABLE Pokemon
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-SET
-    pokemon_id = @pokedex_number, 
-    generation = @generation, 
-    type1 = @type1, 
-    type2 = @type2, 
-    health = @hp, 
-    special_attack = @sp_attack, 
-    attack = @attack, 
-    defense = @defense, 
-    special_defense = @special_defense, 
-    speed = @speed, 
-    height = @height, 
-    weight = @weight;'''
-
-cursor.execute(sql2)
-
-sql3 = '''SELECT * FROM Pokemon;'''
-cursor.execute(sql3) 
-for i in cursor.fetchall(): 
-    print(i)
-
-conn.commit() 
-conn.close() 
-
 @app.route('/')
 def get():
     return 'Pikadle API'
--- backend/db/Pokemon.py
+++ backend/db/Pokemon.py
@@ -13,7 +13,26 @@ class Pokemon(Resource):
     def get(self, pokeID):
         # Get the pokemon from the database
         cur = conn.cursor()
-        cur.execute("SELECT * FROM pokemon WHERE id = %s", (pokeID,))
+        cur.execute("SELECT * FROM pokemon WHERE pokemon_id = %s", (pokeID,))
         pokemon = cur.fetchone()
         cur.close()
-        return pokemon, 200        
+        if pokemon:
+            pokemon_data = {
+                "pokemon_id": int(pokemon[0]),
+                "generation": int(pokemon[1]),
+                "type1": str(pokemon[2]),
+                "type2": str(pokemon[3]),
+                "health": int(pokemon[4]),
+                "special_attack": int(pokemon[5]),
+                "attack": int(pokemon[6]),
+                "defense": int(pokemon[7]),
+                "special_defense": int(pokemon[8]),
+                "speed": int(pokemon[9]),
+                "height": float(pokemon[10]),
+                "weight": float(pokemon[11]),
+                "name": str(pokemon[12])
+                # Add other fields as needed
+            }
+            return pokemon_data, 200
+        else:
+            return {"message": "Pokemon not found"}, 404
