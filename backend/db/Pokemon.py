0ea47972be7991530a0630b480f0e3ac8f7e5cc0
Josh Chen <joshchen2003@gmail.com>
2023-10-04 20:41:45 -0400

    first endpoint

--- backend/db/Pokemon.py
+++ backend/db/Pokemon.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, reqparse
-
+import psycopg2
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
+url = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(url)
 
 class Pokemon(Resource):
     def get(self, pokeID):
         # Get the pokemon from the database
-        return "Pokemon", 200
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM pokemon WHERE id = %s", (pokeID,))
+        pokemon = cur.fetchone()
+        cur.close()
+        return pokemon, 200        
