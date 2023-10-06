208c9bc331bddbcefd489e1717537d5e5108adf4
Olivia Chen <oyc4@duke.edu>
2023-10-05 23:45:26 -0400

    shitty user page

--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -1,10 +1,32 @@
 "use client"
 
-export default function User () {
+import { useEffect, useState } from "react"
+import config from '../../config.json'
 
+
+export default function User () {
+    const [user, setUser] = useState()
+
+    let user1 = {username: "hello", uid: 1, email: "whatever"}
+
+    const fetchUser = () =>{
+        fetch(config.USER_URL + "1")
+            .then(response => response.json())
+            .then((data) => {
+                setUser(data)
+            })
+    }
+    useEffect(() =>{
+        fetchUser()
+    }, [])
     return (
         <div>
-            User
+            <div id='username'>{user1.username}</div>
+            <div id="uid">{user1.uid}</div>
+            <div id = "email">{user1.email}</div>
+            {/* <div id='username'>{user.username}</div>
+            <div id="uid">{user.uid}</div>
+            <div id = "email">{user.email}</div> */}
         </div>
     )
 }
