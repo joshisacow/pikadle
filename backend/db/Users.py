59ae455f09a5690dc51451ebbc598db9be00644d
Josh Chen <joshchen2003@gmail.com>
2023-10-06 00:18:33 -0400

    finished users endpoint

--- backend/db/Users.py
+++ backend/db/Users.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, reqparse
 import psycopg2
 from dotenv import load_dotenv
 import os
+import json
 
 app = Flask(__name__)
 load_dotenv()
@@ -12,15 +13,13 @@ class Users(Resource):
     def get(self):
         # get user info
         # parse arguments
-        argparse = reqparse.RequestParser()
-        argparse.add_argument("uid", type=int, help="User ID is required", required=True)
-        args = argparse.parse_args()
-        userID = args['uid']
+        args = request.args.get('uid')
         conn = psycopg2.connect(url)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM Users WHERE uid = %s", (userID,))
+        cur.execute("SELECT * FROM Users WHERE uid = %s", (args,))
         user = cur.fetchone()
         cur.close()
+        
         return user, 200
 
     def post(self):
@@ -31,6 +30,7 @@ class Users(Resource):
         cur = conn.cursor()
         cur.execute("""INSERT INTO Users (uid, username, email, number_of_pokemon, number_of_badges) 
 VALUES (%s, %s, %s, %s, %s);""", (content['uid'], content['username'], content['email'], content['number_of_pokemon'], content['number_of_badges']))
+        conn.commit()
         cur.close()
         return content, 201
         
