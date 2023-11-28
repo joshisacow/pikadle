77bc07d6b420990622444b16aa39303936bb15fa
Olly Chen <oyc4@duke.edu>
2023-11-28 12:52:02 -0500

    working sprites

--- frontend/src/components/endModal.js
+++ frontend/src/components/endModal.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 
 export default function EndModal ({correct, pokemon, guesses}){
+    const [pokeSprite, setPokeSprite] = useState(null)
+    const fetchSprite = () => {
+        fetch(` https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+            .then((response) => {
+                if (response.ok){
+                    return response.json()
+                }
+                throw new Error('PokeAPI not available')
+            })
+            .then((data) =>{
+                console.log(data.sprites.front_default)
+                setPokeSprite(data.sprites.front_default)
+            })
+    }
+    useEffect(()=>{
+        fetchSprite();
+    }, [])
     const handleClick = () => {
         document.getElementById("exampleModalCenter").style.display= "none";
     }
@@ -21,12 +39,13 @@ export default function EndModal ({correct, pokemon, guesses}){
                 <div className="modal-content">
                 <div className="modal-header">
                     <h5 className="modal-title" id="exampleModalLongTitle">{modaltitle}</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleClick}>
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div className="modal-body">
                     {modalbody}
+                    <img src={pokeSprite} alt="image unavailable"></img>
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={handleClick}>Close</button>
