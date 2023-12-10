c2ed17a8c54936fe465a85b5efc2af10ba1b003b
Olly Chen <oyc4@duke.edu>
2023-12-09 20:06:12 -0500

    fix dailyguess

--- backend/db/Users.py
+++ backend/db/Users.py
@@ -59,7 +59,7 @@ class CanGuess(Resource):
             if today <= user[0]:
                 can_attempt = False
 
-        return True, 200
+        return can_attempt, 200
     
     def post(self, uid):
         # update last attempted date to today
